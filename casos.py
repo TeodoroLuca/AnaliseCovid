b7597f136e178b520568c50384e1477fe6d63902
Lucas Teodoro <130412728+TeodoroLuca@users.noreply.github.com>
2024-09-01 16:35:02 -0300

    Update casos.py

--- casos.py
+++ casos.py
@@ -12,3 +12,22 @@ with open("/home/theodoro/Desktop/analise/casos.csv", 'r', encoding='iso8859-1')
             total_confirmados += 1 # faz o incremento de casos e retorna o total
     os.system('clear')
     print(Fore.BLUE,f"\nTOTAL DE CASOS CONFIRMADOS NO BAIRRO CIDADE INDUSTRIAL DE CURITIBA: {total_confirmados}\n") # printa o total de casos de um bairro especifico
+
+
+# CÓDIGO COM INPUT=ENTRADA DO NOME DO BAIRRO
+from colorama import Fore
+import os
+
+with open("/home/theodoro/Desktop/analise/casos.csv", 'r', encoding='iso8859-1') as casos: # Abrindo o arquivo CSV com encoding iso8859-1: alfabeto latino
+    numero_casos_confirmados = []
+    total_confirmados = 0
+    print(Fore.LIGHTYELLOW_EX)
+    bairro = input("DIGITE O BAIRRO: ").strip().upper() #entrada do nome do bairro desejado
+    
+    for linha in casos:
+        caso = linha.strip().split(';')  # Remove espaços em branco e dividir por ';'
+        if "CONFIRMADO" in caso[1] and caso[4].strip() == bairro:  # Verifica se é confirmado e se o bairro é o pedido
+            print(Fore.WHITE,"DATA DE ATENDIMENTO:", caso[0],Fore.GREEN, "COVID:", caso[1],Fore.RED, "BAIRRO:", caso[4]) # imprimi cada coluna solicitada
+            total_confirmados += 1 # faz o incremento de casos e retorna o total
+    os.system('clear')
+    print(Fore.BLUE,f"\nTOTAL DE CASOS CONFIRMADOS NO BAIRRO: ", bairro, total_confirmados ) # printa o total de casos de um bairro especifico
